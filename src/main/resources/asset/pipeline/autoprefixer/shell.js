c04615278c957833562fc5c62bf0b8a827f4e1b3
Alexey Zhokhov <alexey@zhokhov.com>
2016-02-10 06:41:19 +1000

    Commented print in shell.js.

--- src/main/resources/asset/pipeline/autoprefixer/shell.js
+++ src/main/resources/asset/pipeline/autoprefixer/shell.js
@@ -1,4 +1,4 @@
 function print(text) {
-	Packages.asset.pipeline.autoprefixer.AutoprefixerProcessor.print(text);
+	// Packages.asset.pipeline.autoprefixer.AutoprefixerProcessor.print(text);
 }
 function quit() {}
